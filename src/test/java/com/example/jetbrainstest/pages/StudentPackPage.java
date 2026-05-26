aa686454ba19ed9deaa479107d52e463ddfeab18
mzolotov1994 <mzolotov1994@gmail.com>
2026-05-26 17:36:18 +0300

    Adding additional checks and describing test cases

--- src/test/java/com/example/jetbrainstest/pages/StudentPackPage.java
+++ src/test/java/com/example/jetbrainstest/pages/StudentPackPage.java
@@ -2,6 +2,7 @@ package com.example.jetbrainstest.pages;
 
 import com.example.jetbrainstest.AllureLogger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 // page_url = https://www.jetbrains.com/academy/student-pack/
@@ -31,6 +33,81 @@ public class StudentPackPage {
     @FindBy(css = "svg[class*='_siteLogo__image']")
     WebElement mainLink;
 
+    @FindBy(xpath = "//button[.//span[text()='Learn more']]")
+    WebElement learnMoreButton;
+
+    @FindBy(xpath = "//a[text()='Developer Recognition']")
+    WebElement developerRecognitionLink;
+
+    @FindBy(xpath = "//footer//a[contains(@href,'instagram')]")
+    WebElement instFutterLink;
+
+    @FindBy(css = "svg[data-test='down-icon']")
+    WebElement productsSelector;
+
+    //Элементы для проверок FAQ
+    @FindBy(xpath = "//span[text()='I didn’t find the answer to my question. What should I do?']")
+    WebElement faqButton;
+
+    @FindBy(xpath = "//*[contains(.,'No worries! You can find more details')]")
+    WebElement faqText;
+
+    //Элементы для проверок поля email
+    @FindBy(css = "button[data-test='submit-button']")
+    WebElement submitButton;
+
+    @FindBy(xpath = "//span[contains(text(),'This field is required')]")
+    WebElement emailRequiredMessage;
+
+    //Элементы для проверок смены языка
+    @FindBy(xpath = "//a[contains(@href,'student-pack')]//span[@data-test='button__content']")
+    WebElement studentButton;
+
+    @FindBy(xpath = "//header//button[@data-test='language-picker']")
+    WebElement changeLanguageButton;
+
+    @FindBy(xpath = "//span[contains(@class,'wt-language-grid__language_row_1') and contains(@class,'wt-language-grid__language_column_3')]")
+    WebElement russianLanguageLink;
+
+    //Элемент для проверки перехода по ссылке Discord
+    @FindBy(xpath = "//a[@href='https://discord.com/invite/jpsDuV37bW']")
+    WebElement discordLink;
+
+    //Элементы для проверки Поиска
+    @FindBy(xpath = "//input[@data-test-id='search-input']")
+    WebElement searchInput;
+
+    @FindBy(css = "button[data-test='site-header-search-action']")
+    WebElement searchIcon;
+
+    @FindBy(css = "span[data-test=\"input__clear-icon\"]")
+    WebElement clearIcon;
+
+    @FindBy(xpath = "//button[@data-test='full-search-button']")
+    WebElement fullSearchButton;
+
+    //Элементы для проверки кнопки Больше в хедере
+    @FindBy(xpath = "//button[@data-test='main-menu-more-button']")
+    WebElement moreButton;
+
+    @FindBy(xpath = "//div[@data-test='dropdown']")
+    WebElement dropdownList;
+
+    //Элементы для проверок кнопки региона в футтере
+    @FindBy(xpath = "//button[@data-test='footer-country-button']")
+    WebElement footerCountryButton;
+
+    @FindBy(xpath = "//button[@data-test='footer-popup-confirm-country']")
+    WebElement popupConfirmButton;
+
+    @FindBy(xpath = "//div[@data-test='select' and .//input[@data-test='search-input']]")
+    WebElement popupSearchCountryInput;
+
+    //Элемент для проверки тогла обратной связи на RU локали
+    @FindBy(css = "#hj-survey-toggle-1")
+    WebElement feedbackToggle;
+
+
     public boolean checkBuyButtonIsClickable() {
         LOG.infoWithScreenshot("Проверка активности кнопки купить");
         return buyButton.isEnabled();
@@ -44,6 +121,158 @@ public class StudentPackPage {
     public void clickMainPageLink() {
         mainLink.click();
         LOG.infoWithScreenshot("Осуществили переход на главную страницу сайта");
+    }
+
+    public boolean checkLearnMoreButtonIsClickable() {
+        learnMoreButton.click();
+        LOG.infoWithScreenshot("Проверка активности кнопки Learn More");
+        return learnMoreButton.isEnabled();
+    }
+
+    public boolean checkDevRecognitionIsDisplayed() {
+        LOG.infoWithScreenshot("Проверка отображения ссылки Developer Description");
+        return developerRecognitionLink.isDisplayed();
+    }
+
+    public void clickDevRecognitionLink() {
+        developerRecognitionLink.click();
+        LOG.infoWithScreenshot("Осуществили переход на страницу Developer Recognition");
+    }
+
+    public void clickInstagramFutterLink() {
+        instFutterLink.click();
+    }
+
+    public boolean checkProductsSelectorIsClickable() {
+        productsSelector.click();
+        LOG.infoWithScreenshot("Нажали на селектор с выбором продукта");
+        return productsSelector.isEnabled();
+    }
+
+    //Методы для тестов раздела FAQ
+    public boolean checkClickFaqButtonIsClickable() {
+        faqButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку в разделе FAQ");
+        return faqButton.isEnabled();
+    }
+
+    public String getFaqText() {
+        return faqText.getText();
+    }
+
+    //Методы для тестов поля email
+    public boolean isEmailRequiredMessageDisplayed() {
+        submitButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку Submit с незаполненным email");
+        return emailRequiredMessage.isDisplayed();
+    }
+
+    //Методы для смены языка
+    public void changeLanguage() {
+        changeLanguageButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку смены языка");
+        russianLanguageLink.click();
+        LOG.infoWithScreenshot("Сменили язык на русский");
+    }
+
+    public String getStudentButtonText() {
+        return studentButton.getText();
+    }
+
+    //Метод для перехода по ссылке Discord
+    public void shouldRedirectToDiscordPage() {
+        discordLink.click();
+        LOG.infoWithScreenshot("Нажали на ссылку для перехода на сайт Discord");
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
+        LOG.infoWithScreenshot("Перешли в новую вкладку");
+    }
+
+    //Методы для проверки Поиска
+    public boolean checkSearchInputIsDisplayed() {
+        searchIcon.click();
+        LOG.infoWithScreenshot("Нажали на иконку лупы в хэдере");
+        return searchInput.isDisplayed();
+    }
+
+    public boolean checkClearIconIsClickable() {
+        searchIcon.click();
+        LOG.infoWithScreenshot("Нажали на иконку лупы в хэдере");
+        String text = "text";
+        searchInput.sendKeys(text);
+        LOG.infoWithScreenshot("Вставили текст в инпут поиска");
+        return clearIcon.isEnabled();
+    }
+
+    public void checkSearchInputAfterClearing() {
+        searchIcon.click();
+        LOG.infoWithScreenshot("Нажали на иконку лупы в хэдере");
+        String text = "text";
+        searchInput.sendKeys(text);
+        LOG.infoWithScreenshot("Вставили текст в инпут поиска");
+        clearIcon.click();
+        LOG.infoWithScreenshot("Нажали на кнопку для очистки инпута поиска");
+    }
+
+    public boolean checkFullSearchButtonIsVisible() {
+        searchIcon.click();
+        LOG.infoWithScreenshot("Нажали на иконку лупы в хэдере");
+        String text = "text";
+        searchInput.sendKeys(text);
+        LOG.infoWithScreenshot("Вставили текст в инпут поиска");
+        return fullSearchButton.isDisplayed();
+    }
+
+    public String getSearchInputText() {
+        return searchInput.getAttribute("value");
+    }
+
+    //Методы для проверки дропдауна в кнопке Больше при изменении разрешения экрана
+    public boolean checkDropdownListIsDisplayed() {
+        changeLanguageButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку смены языка");
+        russianLanguageLink.click();
+        LOG.infoWithScreenshot("Сменили язык на русский");
+        moreButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку Больше в хедере");
+        return dropdownList.isDisplayed();
+    }
+
+    //Методы для проверки кнопки с регионом, в футтере
+    public boolean checkPopupConfirmButtonIsClickable() {
+        footerCountryButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку c выбором страны/региона");
+        return popupConfirmButton.isEnabled();
+    }
+
+    public void countryIsNotAppliedWithoutConfirmation() {
+        String countryName = "Austria";
+
+        footerCountryButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку c выбором страны/региона");
+
+        popupSearchCountryInput.sendKeys(countryName);
+        LOG.infoWithScreenshot("Ввели название страны");
+
+        popupConfirmButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку Подтвердить");
+    }
+
+    public String getFooterCountryButtonText() {
+        return footerCountryButton.getText();
+    }
+
+    public void setOtherResolution() {
+        driver.manage().window().setSize(new Dimension(1270, 677));
+    }
+
+    //Методы для проверки тогла обратной связи на RU локали
+    public boolean checkFeedbackToggleIsVisible() {
+        changeLanguageButton.click();
+        LOG.infoWithScreenshot("Нажали на кнопку смены языка");
+        russianLanguageLink.click();
+        LOG.infoWithScreenshot("Сменили язык на русский");
+        return feedbackToggle.isDisplayed();
     }
 
     public void acceptCookies() {
--- src/test/java/com/example/jetbrainstest/tests/StudentPackTest.java
+++ src/test/java/com/example/jetbrainstest/tests/StudentPackTest.java
@@ -45,4 +45,134 @@ public class StudentPackTest extends BaseTest {
 
         assertEquals(expectedUrl, actualUrl, "Переход на главную страницу не осуществлен");
     }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Learn more")
+    public void checkLearnMoreButtonIsClickable() {
+        assertTrue(studentPackPage.checkLearnMoreButtonIsClickable(), "Кнопка Learn more не активна");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения ссылки Developer Description")
+    public void checkDevRecognitionIsDisplayed() {
+        assertTrue(studentPackPage.checkDevRecognitionIsDisplayed(), "Ссылка Developer Recognition отсутствует");
+    }
+
+    @Test
+    @DisplayName("Переход на страницу Developer Recognition")
+    public void clickDevRecognitionLink() {
+        studentPackPage.clickDevRecognitionLink();
+        String actualUrl = getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.jetbrains.com/community/dev-recognition/";
+
+        assertEquals(expectedUrl, actualUrl, "Переход на страницу Developer Recognition не осуществлен");
+    }
+
+    @Test
+    @DisplayName("Переход на страницу Instagram jetbrains из ссылки в футтере")
+    public void clickInstagramFutterLink() {
+        studentPackPage.clickInstagramFutterLink();
+        String actualUrl = getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.instagram.com/jetbrains/";
+
+        assertEquals(expectedUrl, actualUrl, "Переход на страницу Instagram jetbrains не осуществлен");
+    }
+
+    @Test
+    @DisplayName("Проверка активности селектора с продуктами в разделе Graduates")
+    public void checkProductsSelectorIsClickable() {
+        assertTrue(studentPackPage.checkProductsSelectorIsClickable(), "Селектор с выбором продуктов не доступен");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки в разделе FAQ")
+    public void checkClickFaqButtonIsClickable() {
+        assertTrue(studentPackPage.checkClickFaqButtonIsClickable(), "Кнопка в разделе FAQ не активна");
+    }
+
+    @Test
+    @DisplayName("Проверка наличия нужного текста после клика на модуль в FAQ")
+    public void checkFaqTextInFaqButton() {
+        studentPackPage.checkClickFaqButtonIsClickable();
+        String faqText = studentPackPage.getFaqText();
+        assertTrue(faqText.contains("No worries! You can find more details"), "В модуле нет нужного текста");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения подсказки, что поле email обязательно для заполнения")
+    public void isEmailRequiredMessageDisplayed() {
+        assertTrue(studentPackPage.isEmailRequiredMessageDisplayed(), "Подсказка, что поле email обязательно не отображается");
+    }
+
+    @Test
+    @DisplayName("Проверка смены языка на RU на кнопке Студенты")
+    public void checkLanguageSwitchWorks() {
+        studentPackPage.changeLanguage();
+        String textAfter = studentPackPage.getStudentButtonText();
+        assertTrue(textAfter.contains("Студентам"));
+    }
+
+    @Test
+    @DisplayName("Проверка перехода на страницу инвайта в кананал Jetbrains в Discord")
+    public void shouldRedirectToDiscordPage() {
+        studentPackPage.shouldRedirectToDiscordPage();
+        String actualUrl = getDriver().getCurrentUrl();
+        String expectedUrl = "https://discord.com/invite/jpsDuV37bW";
+
+        assertEquals(expectedUrl, actualUrl, "Переход на страницу инвайта в кананал Jetbrains в Discord не осуществлен");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения инпута Поиск")
+    public void checkFeedBackToggleIsClickable() {
+        assertTrue(studentPackPage.checkSearchInputIsDisplayed(), "Инпут для поиска не отображается");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения инпута Поиск")
+    public void checkClearIconIsClickable() {
+        assertTrue(studentPackPage.checkClearIconIsClickable(), "Кнопка очистки инпута поиска не активна");
+    }
+
+    @Test
+    @DisplayName("Проверка очистки инпута поиска")
+    public void checkSearchInputAfterClearing() {
+        studentPackPage.checkSearchInputAfterClearing();
+        assertTrue(studentPackPage.getSearchInputText().isEmpty(), "Инпут поиска содержит значение");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения кнопки Расширенного поиска")
+    public void checkFullSearchButtonIsVisible() {
+        assertTrue(studentPackPage.checkFullSearchButtonIsVisible(), "Кнопка для расширенного поиска не отображается");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения дропдаун списка в кнопке Больше при изменении разрешения экрана")
+    public void checkDropdownListIsDisplayed() {
+        studentPackPage.setOtherResolution();
+        assertTrue(studentPackPage.checkDropdownListIsDisplayed(), "Дропдаун список не отображается");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Выбрать в поп-апе с выбором страны/региона")
+    public void checkPopupConfirmButtonIsClickable() {
+        assertTrue(studentPackPage.checkPopupConfirmButtonIsClickable(), "Кнопка Выбрать в поп-апе выбора страны/региона не активна");
+    }
+
+    @Test
+    @DisplayName("Проверка, что наименование страны в кнопке не изменилось без подтверждающего действия")
+    public void countryIsNotAppliedWithoutConfirmation() {
+        studentPackPage.countryIsNotAppliedWithoutConfirmation();
+        String expectedButtonName = studentPackPage.getFooterCountryButtonText();
+        String actualButtonName = studentPackPage.getFooterCountryButtonText();
+        assertEquals(expectedButtonName, actualButtonName, "Наименование страны в кнопке изменилось");
+    }
+
+    @Test
+    @DisplayName("Проверка отображения тогла обратной связи на RU локали")
+    public void checkFeedbackToggleIsVisible() {
+        assertTrue(studentPackPage.checkFeedbackToggleIsVisible(), "Тогл обратной связи не отображается на странице");
+    }
+
 }
